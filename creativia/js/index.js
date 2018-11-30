b005e28a9f69ce165e4fdf8a78f570fe9ff9af8a
Nataliia Bilan <natali_bilan@ukr.net>
2018-11-30 12:05:06 +0200

    added to-page-nav

--- creativia/js/index.js
+++ creativia/js/index.js
@@ -1,20 +1,20 @@
 'use strict'
-$(function(){
+$(function () {
     var $page = $('html, body');
-    $('.main-menu a[href*="#"]').click(function(){ 
+    $('.main-menu a[href*="#"], .to-page-nav').click(function () {
         $page.animate({
-        scrollTop: $($.attr(this, 'href')).offset().top - 99
-    }, 700);
-    return false;
-});
-    $('.fa-minus').click(function(){
+            scrollTop: $($.attr(this, 'href')).offset().top - 99
+        }, 700);
+        return false;
+    });
+    $('.fa-minus').click(function () {
         $(this).parent().siblings('.who-is').hide(1500);
         $(this).parent().children('.fa-plus').show();
         $(this).parent().children('.subtitle, .fa-minus').css('color', '#262626');
         $(this).parent().children('.fa-minus').hide();
         return false;
     });
-    $('.fa-plus').click(function(){
+    $('.fa-plus').click(function () {
         $(this).parent().siblings('.who-is').fadeToggle(1500);
         $(this).parent().children('.subtitle, .fa-minus').css('color', 'red');
         $(this).parent().children('.fa-minus').show();
@@ -22,11 +22,11 @@ $(function(){
         return false;
     });
 
-    $('#nav-toggle, .main-menu li a').click(function(){
+    $('#nav-toggle, .main-menu li a').click(function () {
         $('.main-menu').toggleClass('menu-open');
-     });
-     $(document).click(function(event) {
-        if ($(event.target).closest("#nav-toggle").length ) return;
+    });
+    $(document).click(function (event) {
+        if ($(event.target).closest("#nav-toggle").length) return;
         $('.main-menu').removeClass('menu-open');
         event.stopPropagation();
     });
